1f06181b9083cc1adde858b08703ecfaa123fa80
matheusmansur694 <matheus.toniazzo01093752@sesisenaipr.org.br>
2025-10-09 12:20:20 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ async function initDB() {
     )`);
 }
 
-app.get('/tasks', async(requestAnimationFrame, res) => {
-    const tasks = await db.all()
+app.get('/tasks', async(req, res) => {
+    const tasks = await db.all(`SELECT * FROM tasks`);
     res.json(tasks)
 });
 
-app.post('/tasks', async(requestAnimationFrame, res) => {
+app.post('/tasks', async(req, res) => {
     const {description} = req.body;
     const stmt = await db.prepare (`INSERT INTO tasks (description) VALUES (?)`);
     await stmt.run(description);
@@ -36,10 +36,23 @@ app.post('/tasks', async(requestAnimationFrame, res) => {
     res.status(201).json({ message: 'Task added'});
 });
 
-app.delete('/tasks', async(req, res) => {
+app.delete('/tasks/:id', async(req, res) => {
         const { id } = req.params;
         await db.get(`DELETE FROM tasks WHERE id = ?`, id);
         res.status(204).send();
+})
+
+app.patch('/task/:id/toggle', async(req, res) => {
+    const { id } = req.params;
+    const task = await db.get(`SELECT * FROM tasks WHERE id = ?`, id);
+    const completed = task.completed ? 0 : 1;
+    await db.run(`UPDATE taks SET completed = ? WHERE id = ?`, completed, id);
+    res.status(204).send();
+});
+
+app.listen(PORT, async() => {
+    await initDB();
+    console.log(`Server running at https://localhost:${PORT}`);
 })
 
 
